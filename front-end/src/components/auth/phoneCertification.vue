20f80bf192ee9fb7d631156227ce1192e5c4613f
dahaelee <dah3132@gmail.com>
2021-04-30 01:23:13 +0900

    Update phoneCertification.vue

--- front-end/src/components/auth/phoneCertification.vue
+++ front-end/src/components/auth/phoneCertification.vue
@@ -65,7 +65,7 @@ export default {
       const apiKey = "89358db6-c434-40fe-9ae2-a2254dc1506a"
       //#자기 키로 변경
       const tmpWindow = window.open("about:blank");
-      tmpWindow.location = "https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id="+apiKey+"&redirect_uri=https://finextend.herokuapp.com/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0"
+      tmpWindow.location = "https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id="+apiKey+"&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0"
     },
     async goToPayment () {
       this.code = this.stateCode      
